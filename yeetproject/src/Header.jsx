3ebdae4fd6e425bef22c90a424ba3853d73368fe
constructorlib <constructorlib@gmail.com>
2023-11-02 14:57:09 +0900

    manual labor is always better than automated

--- yeetproject/src/Header.jsx
+++ yeetproject/src/Header.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faMagnifyingGlass,
+  faBell,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 function Header() {
   return (
     <div className="header-all">
@@ -15,8 +21,14 @@ function Header() {
           <a href="#">Community</a>
           <a href="#">Calendar</a>
         </div>
-        <div className="navbar-icons"></div>
+        <div className="navbar-icons">
+          <FontAwesomeIcon className="faicon" icon={faMagnifyingGlass} />
+          <FontAwesomeIcon className="faicon" icon={faBell} />
+          <FontAwesomeIcon className="faicon" icon={faUser} />
+        </div>
       </div>
+      <br />
+      <hr />
     </div>
   );
 }
