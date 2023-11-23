2ca006f58fdaab5b6fccc7c39f46639709f5a023
constructorlib <constructorlib@gmail.com>
2023-11-23 10:45:53 +0900

    deadline very soon

--- yeetproject/src/main.jsx
+++ yeetproject/src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
-import Header from "./Header.jsx";
-import "./index.css";
+import Header from "./components/header/Header";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
