67e0b36ef495736214f02ab8f3485224ea2393cd
constructorlib <constructorlib@gmail.com>
2023-11-24 11:30:45 +0900

    dumb mistake

--- yeetproject/src/main.jsx
+++ yeetproject/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
-import Header from "./components/header/Header";
+// import Header from "./components/header/Header";
+import Registration from "./pages/Registration/Registration";
 import "./style.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Header />
+    {/* <Header /> */}
   </React.StrictMode>
 );
---
+++ yeetproject/src/pages/Registration/Registration.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../../components/header/Header";
+export default function Registration() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
