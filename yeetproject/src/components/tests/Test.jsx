653657005e29da982eb3acb235f37f8e378a4605
constructorlib <constructorlib@gmail.com>
2023-11-24 15:43:51 +0900

    stupid power logic (change it later)

--- yeetproject/src/components/tests/Test.jsx
+++ yeetproject/src/components/tests/Test.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./test.css";
+
+const Exponent = ({ base, power }) => (
+  <span>
+    {base}
+    <sup>{power}</sup>
+  </span>
+);
+
 export default function Test() {
   return (
     <div>
       <div className="testHeader"></div>
       <div className="testWrapper">
         <div className="card">
-          <div className="cardQuestion"></div>
+          <div className="cardQuestion">
+            <p>
+              <Exponent base="Ushbu y = x" power="4" />
+              <Exponent base=" va y = 2x" power="2" />
+            </p>
+          </div>
           <div className="cardAnswer"></div>
         </div>
       </div>
