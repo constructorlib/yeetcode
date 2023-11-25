ddae8e6ae4263f6cccd2517024b5171c64e11054
constructorlib <constructorlib@gmail.com>
2023-11-25 16:29:58 +0900

    advice thing

--- yeetproject/src/components/tests/Test.jsx
+++ yeetproject/src/components/tests/Test.jsx
@@ -44,7 +44,7 @@ export default function Test() {
         <div className="testPassed">
           <div className="passWrapper">
             <div className="passTitle">
-              <h4>3/9 tests passed, 6 more tests left!</h4>
+              <h4>6/11 tests passed, 5 more tests left!</h4>
             </div>
             <div className="passCredit">
               <div className="correct">1</div>
@@ -53,11 +53,20 @@ export default function Test() {
               <div>4</div>
               <div className="correct">5</div>
               <div>6</div>
-              <div>7</div>
+              <div className="correct">7</div>
               <div>8</div>
               <div className="correct">9</div>
-              <div>10</div>
-              <div>11</div>
+              <div className="correct">10</div>
+              <div className="correct">11</div>
+            </div>
+          </div>
+          <div className="improve">
+            <h4>Here's what you could do to improve your score!</h4>
+            <div className="advice">
+              <p>1) Git gud, lol</p>
+              <p>2) Git gud, lol</p>
+              <p>3) Git gud, lol</p>
+              <p>4) Git gud, lol</p>
             </div>
           </div>
         </div>
