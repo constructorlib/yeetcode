7a587f17b7bd5ddea7749c93d450dfed4578752f
constructorlib <constructorlib@gmail.com>
2023-11-25 15:29:41 +0900

    test

--- yeetproject/src/main.jsx
+++ yeetproject/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 import Test from "./components/tests/Test";
-// import Registration from "./pages/Registration/Registration";
+import Registration from "./pages/Registration/Registration";
 import "./style.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Registration /> */}
-    <Test />
+    <Registration />
+    {/* <Test /> */}
   </React.StrictMode>
 );
