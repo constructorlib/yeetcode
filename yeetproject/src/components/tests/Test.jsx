4234c8b966d04f83192b74a81c15041c8b17e011
constructorlib <constructorlib@gmail.com>
2023-11-25 15:20:12 +0900

    inital logic (manual data)

--- yeetproject/src/components/tests/Test.jsx
+++ yeetproject/src/components/tests/Test.jsx
@@ -7,26 +7,39 @@ const Exponent = ({ base, power }) => (
     <sup>{power}</sup>
   </span>
 );
-
+const Savol = () => {
+  return (
+    <div className="cardQuestion">
+      <p>
+        <Exponent base="Ushbu y = x" power="4" />
+        <Exponent base=" va y = 2x" power="2" />
+        <Exponent base=" grafikni umumiy nuqtasi nechta?" />
+      </p>
+    </div>
+  );
+};
+const Javob = () => {
+  return (
+    <div className="cardAnswer">
+      <p>a) 3</p>
+      <p>b) 2</p>
+      <p>c) 1</p>
+      <p>d) 4</p>
+    </div>
+  );
+};
 export default function Test() {
   return (
     <div>
       <div className="testHeader"></div>
       <div className="testWrapper">
         <div className="card">
-          <div className="cardQuestion">
-            <p>
-              <Exponent base="Ushbu y = x" power="4" />
-              <Exponent base=" va y = 2x" power="2" />
-              <Exponent base=" grafikni umumiy nuqtasi nechta?" />
-            </p>
-          </div>
-          <div className="cardAnswer">
-            <p>a) 3</p>
-            <p>b) 2</p>
-            <p>c) 1</p>
-            <p>d) 4</p>
-          </div>
+          <Savol />
+          <Javob />
+          <Savol />
+          <Javob />
+          <Savol />
+          <Javob />
         </div>
       </div>
     </div>
