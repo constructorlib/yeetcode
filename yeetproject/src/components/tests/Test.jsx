319041f85a618ceda29a4f2b8b3f9d11ccaf3817
constructorlib <constructorlib@gmail.com>
2023-11-25 15:15:53 +0900

    even more styling

--- yeetproject/src/components/tests/Test.jsx
+++ yeetproject/src/components/tests/Test.jsx
@@ -18,13 +18,14 @@ export default function Test() {
             <p>
               <Exponent base="Ushbu y = x" power="4" />
               <Exponent base=" va y = 2x" power="2" />
+              <Exponent base=" grafikni umumiy nuqtasi nechta?" />
             </p>
           </div>
           <div className="cardAnswer">
-            <p>1) 1</p>
-            <p>2) 2</p>
-            <p>3) 3</p>
-            <p>4) 4</p>
+            <p>a) 3</p>
+            <p>b) 2</p>
+            <p>c) 1</p>
+            <p>d) 4</p>
           </div>
         </div>
       </div>
