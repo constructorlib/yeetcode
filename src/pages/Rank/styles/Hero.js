5604e39eb86e266a21b8d48fbc97485cc7e5c79d
typosbro <typosbro@proton.me>
2023-11-27 21:53:42 +0900

    skeleton

---
+++ src/pages/Rank/styles/Hero.js
@@ -0,0 +1,16 @@
+import styled, { css } from "styled-components";
+import SVG from "react-inlinesvg";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Icon = styled(SVG)`
+  width: 4rem;
+  height: 4rem;
+`;
+export const Main = styled(SVG)`
+  width: 6rem;
+  height: 6rem;
+`;
