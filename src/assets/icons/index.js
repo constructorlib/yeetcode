27bdd85db92b4f202f9664d9083e0c047f862eb2
typosbro <typosbro@proton.me>
2023-11-27 19:11:36 +0900

    add header icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,8 @@
+export { default as bio } from "./bio.svg";
+export { default as chem } from "./chem.svg";
+export { default as diamond } from "./diamond.svg";
+export { default as heart } from "./heart.svg";
+export { default as math } from "./math.svg";
+export { default as physics } from "./physics.svg";
+export { default as streak } from "./streak.svg";
+export { default as tech } from "./tech.svg";
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,27 @@
-import { Container, Item } from "./styled";
+import { Container, Item, Icon, Span, Course, Bar, List } from "./styled";
+import { bio, streak, diamond, heart } from "assets/icons/";
 
 const Header = () => {
   return (
     <Container>
-      <Item>1</Item>
-      <Item>2</Item>
-      <Item>3</Item>
-      <Item>4</Item>
+      <List>
+        <Item>
+          <Course src={bio} alt="bio" />
+        </Item>
+        <Item>
+          <Icon src={streak} alt="streak" />
+          <Span>12</Span>
+        </Item>
+        <Item>
+          <Icon src={diamond} alt="diamond" />
+          <Span>618</Span>
+        </Item>
+        <Item>
+          <Icon src={heart} alt="heart" />
+          <Span>5</Span>
+        </Item>
+      </List>
+      <Bar />
     </Container>
   );
 };
