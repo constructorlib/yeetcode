e31c7cd8d4a1a77c820258713c2478196d90f490
typosbro <typosbro@proton.me>
2023-11-27 16:45:43 +0900

    cards ready

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,32 +1,48 @@
 import { Container, Card, List, Title, Subtitle, Progress, Button } from "./styled";
 
+const cards = [
+  {
+    title: "Section 1: Rookie",
+    done: 5,
+    total: 8,
+  },
+  {
+    title: "Section 2: Explorer",
+    done: 0,
+    total: 20,
+  },
+  {
+    title: "Section 3: Traveler",
+    done: 0,
+    total: 28,
+  },
+  {
+    title: "Section 4: Champion",
+    done: 0,
+    total: 8,
+  },
+  {
+    title: "Section 5: Refresh",
+    done: 0,
+    total: 6,
+  },
+];
+
 const Menu = () => {
   return (
     <Container>
       <List>
-        <Card>
-          <Title>Section 1: Rookie</Title>
-          <Subtitle>5/8 UNITS</Subtitle>
-          <Progress />
-          <Button>SEE DETAILS</Button>
-          <Button>CONTINUE</Button>
-        </Card>
-
-        <Card>
-          <Title>Section 1: Rookie</Title>
-          <Subtitle>5/8 UNITS</Subtitle>
-          <Progress />
-          <Button>SEE DETAILS</Button>
-          <Button>CONTINUE</Button>
-        </Card>
-
-        <Card>
-          <Title>Section 1: Rookie</Title>
-          <Subtitle>5/8 UNITS</Subtitle>
-          <Progress />
-          <Button>SEE DETAILS</Button>
-          <Button>CONTINUE</Button>
-        </Card>
+        {cards.map((c) => (
+          <Card>
+            <Title>{c.title}</Title>
+            <Subtitle>
+              {c.done}/{c.total} UNITS
+            </Subtitle>
+            <Progress />
+            <Button bg="light">SEE DETAILS</Button>
+            <Button bg="dark">CONTINUE</Button>
+          </Card>
+        ))}
       </List>
     </Container>
   );
--- src/pages/Menu/styled.js
+++ src/pages/Menu/styled.js
@@ -1,3 +1,4 @@
+import { css } from "@emotion/react";
 import styled from "styled-components";
 import { typography } from "styles/";
 
@@ -6,16 +7,14 @@ export const Container = styled.div`
   height: max-content;
 
   position: relative;
-  background-color: var(--primary);
+  background-color: var(--secondary-bg);
 `;
 
 export const List = styled.div`
   display: flex;
   width: 100vw;
-
   gap: 2rem;
   padding: 5rem 0 5rem 5rem;
-
   overflow-x: auto;
 `;
 
@@ -29,21 +28,37 @@ export const Card = styled.div`
   /* gap: 1.5rem; */
 
   width: 75vw;
-  height: 27rem;
-  border-radius: 15px;
-  background-color: var(--button);
+  height: 30rem;
+  border-radius: 2rem;
+  background-color: var(--secondary-bg-darker);
 `;
 export const Title = styled.div`
   ${typography.headingMd}
+  color: var(--dark-text);
 `;
 export const Subtitle = styled.div`
   ${typography.displayMd}
+  color: var(--dark-text);
   text-transform: capitalize;
 `;
+
 export const Button = styled.div`
   ${typography.buttonLg}
-
   text-transform: capitalize;
+  ${({ bg }) => console.log(bg)}
+
+  display: grid;
+  place-content: center;
+
+  width: 100%;
+  height: 4rem;
+  border-radius: 1.5rem;
+  box-shadow: 0px 5px 10px -3px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 0px 5px 10px -3px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 5px 10px -3px rgba(0, 0, 0, 0.75);
+
+  background-color: ${(props) => (props.bg === "dark" ? "var(--dark-bg)" : "var(--secondary-bg)")};
+  color: ${(props) => (props.bg === "dark" ? "var(--secondary-bg-darker)" : "var(--dark-text)")};
 `;
 export const Progress = styled.div`
   text-transform: capitalize;
