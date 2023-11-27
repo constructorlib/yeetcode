8e3d8b00a39b27679e36a0057cd079ae78d9744b
typosbro <typosbro@proton.me>
2023-11-27 20:32:39 +0900

    skeleton pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,6 @@
 export { default as Menu } from "./Menu";
+export { default as Notifications } from "./Notifications";
+export { default as Profile } from "./Profile";
+export { default as Rank } from "./Rank";
+export { default as Rewards } from "./Rewards";
 export { default as Test } from "./Test";
