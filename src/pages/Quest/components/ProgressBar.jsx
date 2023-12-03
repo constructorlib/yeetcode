bc210906457af732e869c9795af03a23e929b259
typosbro <typosbro@proton.me>
2023-12-04 02:36:32 +0900

    progressbar

---
+++ src/pages/Quest/components/ProgressBar.jsx
@@ -0,0 +1,7 @@
+import { Container } from "../styles/ProgressBar";
+
+const ProgressBar = () => {
+  return <Container>ProgressBar</Container>;
+};
+
+export default ProgressBar;
---
+++ src/pages/Quest/styles/ProgressBar.js
@@ -0,0 +1,15 @@
+import SVG from "react-inlinesvg";
+import styled from "styled-components";
+import { typography } from "styles/";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 2rem;
+  background-color: #448b2d;
+`;
+export const Subtitle = styled.div``;
+export const Title = styled.h1``;
+export const Span = styled.span``;
